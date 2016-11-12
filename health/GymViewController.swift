47f248a78defa17ab20477f7e0ecb45eabe741ce
Jim <jimhollingworth1@gmail.com>
2016-11-12 03:56:04 -0800

    added animation

--- health/GymViewController.swift
+++ health/GymViewController.swift
@@ -12,9 +12,27 @@ class GymViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imagesNames = ["bicepcurl0001", "bicepcurl0002", "bicepcurl0003",
+                           "bicepcurl0004",
+                           "bicepcurl0005",
+                           "bicepcurl0006",
+                           "bicepcurl0007",
+                           "bicepcurl0008",
+                           "bicepcurl0009"]
+        var images = [UIImage]()
+        
+        for i in 0..<imagesNames.count {
+            images.append(UIImage(named: imagesNames[i])!)
+        }
+        
+        imageView.animationImages = images
+        imageView.animationDuration = 0.75
+        imageView.startAnimating()
 
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var imageView: UIImageView!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
