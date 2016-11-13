cd88734c164253563faa8abd2c0a02068a921d76
Sean Chen <sean@Seans-MacBook-Pro.local>
2016-11-12 16:24:35 -0800

    added homescreen picture framework

--- health/ViewController.swift
+++ health/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var level: UILabel!
     @IBOutlet weak var whiteBar: UIImageView!
     @IBOutlet weak var greenBar: UIImageView!
+    @IBOutlet weak var homeScreenDude: UIImageView!
 
 
     override func viewDidLoad() {
@@ -34,6 +35,11 @@ class ViewController: UIViewController {
         NSLayoutConstraint(item: whiteBar, attribute: .width, relatedBy: .equal, toItem: greenBar, attribute:.width, multiplier: CGFloat(percentEmpty), constant:-6.0).isActive = true
         NSLayoutConstraint(item: whiteBar, attribute: .height, relatedBy: .equal, toItem: greenBar, attribute:.height, multiplier: 1.0, constant:-6.0).isActive = true
         NSLayoutConstraint(item: whiteBar, attribute: .trailing, relatedBy: .equal, toItem: greenBar, attribute:.trailing, multiplier: 1.0, constant:-3.0).isActive = true
+        
+        
+        homeScreenDude.animationImages = findImages(level: lv)
+        homeScreenDude.animationDuration = 0.75
+        homeScreenDude.startAnimating()
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,6 +53,43 @@ class ViewController: UIViewController {
 struct defaultsKeys {
     static let keyLevel = "levelStringKey"
     static let keyEXP = "expStringKey"
+}
+
+func findImages(level: Int) -> Array<UIImage>{
+    var imageNames = [String]()
+    print(level/5)
+    switch level/5{
+    case 1:
+        imageNames = ["bicepcurl0001", "bicepcurl0002", "bicepcurl0003",
+                      "bicepcurl0004",
+                      "bicepcurl0005",
+                      "bicepcurl0006",
+                      "bicepcurl0007",
+                      "bicepcurl0008",
+                      "bicepcurl0009"]
+        break
+    default: // levels 1-4
+        imageNames = ["chillin0001", "chillin0002", "chillin0003",
+                      "chillin0004",
+                      "chillin0005",
+                      "chillin0006",
+                      "chillin0007",
+                      "chillin0008",
+                      "chillin0010",
+                      "chillin0011",
+                      "chillin0012"]
+        
+        
+        
+    }
+    
+    var images = [UIImage]()
+    
+    for i in 0..<imageNames.count {
+        images.append(UIImage(named: imageNames[i])!)
+    }
+    
+    return images
 }
 
 func gainExp(amount: Int){
