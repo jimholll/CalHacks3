40843d13f4888264449332c1ca6ee87ffdd02596
Jim <jimhollingworth1@gmail.com>
2016-11-12 21:14:47 -0800

    added levelup animation

--- LevelUpViewController.swift
+++ LevelUpViewController.swift
@@ -9,15 +9,50 @@
 import UIKit
 
 class LevelUpViewController: UIViewController {
+    @IBOutlet weak var levelupDude: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var imagesNames = ["levelup0001", "levelup0002", "levelup0003",
+                           "levelup0004",
+                           "levelup0005",
+                           "levelup0006",
+                           "levelup0007",
+                           "levelup0008",
+                           "levelup0009",
+                           "levelup0010", "levelup0011", "levelup0012",
+                           "levelup0013",
+                           "levelup0014",
+                           "levelup0015",
+                           "levelup0016",
+                           "levelup0017",
+                           "levelup0018",
+                           "levelup0019",
+                           "levelup0020",
+                           "levelup0021", "levelup0022", "levelup0023",
+                           "levelup0024",
+                           "levelup0025",
+                           "levelup0026",
+                           "levelup0027",
+                           "levelup0028",
+                           "levelup0029"
+        ]
+        var images = [UIImage]()
+        
+        for i in 0..<imagesNames.count {
+            images.append(UIImage(named: imagesNames[i])!)
+        }
+        
+        levelupDude.animationImages = images
+        levelupDude.animationDuration = 2.4
+        levelupDude.startAnimating()
 
         // Do any additional setup after loading the view.
         
         
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
             // your code here
             self.performSegue(withIdentifier: "fromLevelUpToHome", sender: self)
         }
